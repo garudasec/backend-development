9272ff6aca250c1a31f040379bbaa5876ae7fda6
garudasec <228494846+garudasec@users.noreply.github.com>
2026-07-26 17:57:26 +0530

    refactor: reorganize database connection logic and remove unused notes

--- notes.md
+++
@@ -1,3 +0,0 @@
-## .env  -> environment variable file
-
-jab cheejo ko production me push karenge,toh ye environment variable system se uthaye jate hai taki, file se nahi taki ye secure rahe
--- src/db/mongoDB.js
+++ src/db/mongoDB.js
@@ -1,17 +1,23 @@
+import dns from "dns";
+// force Node.js to use reliable public DNS servers
+dns.setServers(['1.1.1.1', '8.8.8.8']); 
+
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const connectDB =  async () => {
+const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
 
         console.log(`\n MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`);
-      
+        
+        
     } catch (error) {
-        console.log("MONGODB connection error ", error);
-        process.exit(1); // current application ko exit kar deta hai jo chal rahi hoti hai
+        console.log("MONGODB connection FAILED", error);
+        process.exit(1)  // process exit code
+        
     }
 }
 
 
-export default connectDB;
+export default connectDB
