b5dfda97eef5aa08526461ac252834928691b3c3
garudasec <228494846+garudasec@users.noreply.github.com>
2026-07-28 22:22:21 +0530

    feat: add user registration route and controller

--- src/app.js
+++ src/app.js
@@ -24,12 +24,25 @@ app.use(
 // in url, sometimes it set space with + or %, so here set encoder so that it set space with %
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-
 // to store public asset like some files/images
 app.use(express.static("public"));
 
 // to access and set the client's cookie
 app.use(cookieParser())
+
+
+
+
+// routes import
+import userRouter from "./routes/user.route.js"
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter )
+
+
+
+
 
 
 export { app };
