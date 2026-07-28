601f78be22623fd1a368531b3ed1b45cceb8abf7
garudasec <228494846+garudasec@users.noreply.github.com>
2026-07-28 10:29:14 +0530

    connect to MongoDB Atlas

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+
+// this use method is used for middleware or configurations
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+
+// data will come from different place like url, json, body, direct form, so we have set limit
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+// in url, sometimes it set space with + or %, so here set encoder so that it set space with %
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+
+// to store public asset like some files/images
+app.use(express.static("public"));
+
+app.use(cookieParser())
+
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 // require('dotenv').config({path: './env'})
 
-import dotenv from "dotenv"
-
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
 import connectDB from "./db/mongoDB.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
-connectDB() 
+  path: "./env",
+});
 
 
 
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed! ", err);
+  });
 
 
 
