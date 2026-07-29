dc9fd3a548223d953a5b5f601afc0dcdcdc9f040
garudasec <228494846+garudasec@users.noreply.github.com>
2026-07-29 11:27:55 +0530

    feat: enhance user registration with validation, image upload, and error handling

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,25 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
+
 
 const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    // accept karta hai array
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),  
+    registerUser)
+
+
 
 export default router;
